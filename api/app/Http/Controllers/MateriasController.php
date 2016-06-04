3dbe308ed77d9f5bfac968d81f9f48181a8b08ab
Andrés Herrera García <andreshg112@gmail.com>
2016-06-04 01:13:37 -0500

    Creada la tabla tutores.

--- api/app/Http/Controllers/MateriasController.php
+++ api/app/Http/Controllers/MateriasController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Materia;
+use App\Models\Materia;
 use \stdClass;
 
 class MateriasController extends Controller
---
+++ api/app/Models/Tutor..php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Tutor extends Model
+{
+    use SoftDeletes;
+    
+    protected $table = 'tutores';
+    protected $fillable = ['primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido'];
+}
