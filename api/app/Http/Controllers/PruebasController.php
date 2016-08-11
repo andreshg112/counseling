7d2bb9a59d90bc72bc3093b969fab7d7cbff6b86
Andrés Herrera García <andreshg112@gmail.com>
2016-08-11 11:36:07 -0500

    Cobra a automóviles y motocicletas correctamente.

--- api/app/Http/Controllers/PruebasController.php
+++ api/app/Http/Controllers/PruebasController.php
@@ -12,108 +12,118 @@ class PruebasController extends Controller
         $datos = $request->all();
         $entrada = Carbon::parse($datos['entrada']);
         $salida = Carbon::parse($datos['salida']);
-        $dias_diferencia = $salida->day - $entrada->day;
-        $puntos = 24 * 60;
-        $minutos_salida = $salida->hour * 60 + $salida->minute;
-        $ultimo = $minutos_salida;
-        $minutos_entrada = $entrada->hour * 60 + $entrada->minute;
-        $inicial = $minutos_entrada;
+        $tipo_vehiculo = $datos['tipo_vehiculo'];
         $valor = 0;
-        //Cobrar llegada
-        if ((7 * 60 + 10) <= $minutos_entrada && $minutos_entrada < (18 * 60 + 10)) {
-            $valor += 1400;
-        } else {
-            $valor += 2500;
-        }
-        for ($i = 0; $i <= $dias_diferencia; $i++) {
-            for ($j = $inicial + 1; $j < $puntos; $j++) {
-                switch ($j) {
-                    case 7 * 60 + 10:
-                        $valor += 1400;
-                        break;
-                    case 18 * 60 + 10:
-                        $valor += 2500;
-                        break;
-                }
-                if ($i == $dias_diferencia && $j == $ultimo) {
-                    break;
-                }
-            }
-            $inicial = 0;
+        if ($tipo_vehiculo == 'motocicleta') {
+            $valor = cobrarMotocicleta($entrada, $salida);
+        } elseif ($tipo_vehiculo == 'automovil') {
+            $valor = cobrarAutomovil($entrada, $salida);
         }
         print_r($valor);
+    }
 
-        /*$datos = $request->all();
-    $entrada = Carbon::parse($datos['entrada']);
-    $salida = Carbon::parse($datos['salida']);
-    $minutos_entrada = $entrada->hour * 60 + $entrada->minute;
-    $minutos_salida = $salida->hour * 60 + $salida->minute;
-    $valor = 0;
+}
+
+function cobrarMotocicleta($entrada, $salida)
+{
     $dias_diferencia = $salida->day - $entrada->day;
-    $puntos = 24 * 60;
+    $minutos_dia = 24 * 60;
+    $minutos_salida = $salida->hour * 60 + $salida->minute;
     $ultimo = $minutos_salida;
+    $minutos_entrada = $entrada->hour * 60 + $entrada->minute;
     $inicial = $minutos_entrada;
+    $valor = 0;
     //Cobrar llegada
     if ((7 * 60 + 10) <= $minutos_entrada && $minutos_entrada < (18 * 60 + 10)) {
-    $valor += 1400;
+        $valor += 1400;
     } else {
-    $valor += 2500;
+        $valor += 2500;
     }
     for ($i = 0; $i <= $dias_diferencia; $i++) {
-    for ($j = $inicial + 1; $j < $puntos; $j++) {
-    switch ($i) {
-    case 7 * 60 + 10:
-    $valor += 1400;
-    break;
+        for ($j = $inicial + 1; $j < $minutos_dia; $j++) {
+            switch ($j) {
+                case 7 * 60 + 10:
+                    $valor += 1400;
+                    break;
+                case 18 * 60 + 10:
+                    $valor += 2500;
+                    break;
+            }
+            if ($i == $dias_diferencia && $j == $ultimo) {
+                break;
+            }
+        }
+        $inicial = 0;
+    }
+    return $valor;
+}
 
-    case 18 * 60 + 10:
-    $valor += 2500;
-    break;
+function cobrarAutomovil($entrada, $salida)
+{
+    $dias_diferencia = $salida->day - $entrada->day;
+    $minutos_dia = 24 * 60;
+    $minutos_salida = $salida->hour * 60 + $salida->minute;
+    $ultimo = $minutos_salida;
+    $minutos_entrada = $entrada->hour * 60 + $entrada->minute;
+    $inicial = $minutos_entrada;
+    $valor = $tiempo_de_noche = 0;
+    $noches_pagadas = [];
+    for ($i = 0; $i <= $dias_diferencia; $i++) {
+        $tiempo_de_dia = 0;
+        for ($j = $inicial; $j < $minutos_dia; $j++) {
+            if ((7 * 60 + 9) == $j) {
+                //echo $tiempo_de_noche . " noche$i <br>";
+                $valor += min(ceil(($tiempo_de_noche) / 60) * 2000, 4000);
+                $tiempo_de_noche = 0;
+            }
+            if ($i == $dias_diferencia && $j == $ultimo) {
+                break;
+            }
+            if ((7 * 60 + 9) <= $j && $j < (18 * 60 + 9)) {
+                $tiempo_de_dia++;
+            } else {
+                $tiempo_de_noche++;
+            }
+        }
+        $inicial = 0;
+        //echo $tiempo_de_dia . " dia$i <br>";
+        $valor += min(ceil(($tiempo_de_dia) / 60) * 2000, 12000);
+        if ($tiempo_de_noche > 0 && $i >= $dias_diferencia) {
+            //echo $tiempo_de_noche . " noche$i <br>";
+            $valor += min(ceil(($tiempo_de_noche) / 60) * 2000, 4000);
+            $tiempo_de_noche = 0;
+        }
     }
-    if ($i == $dias_diferencia && $j == $ultimo) {
-    break;
-    }
-    }
-    $inicial = 0;
-    }
-    print_r($valor);*/
-    }
-
-    /*if ($salida->day > $entrada->day) {
-$max = 24 * 60;
+    //echo $entrada->diffInMinutes($salida) . " dif<br>";
+    return $valor;
+}
+/*
+//Cobrar llegada
+if ((7 * 60 + 10) <= $minutos_entrada && $minutos_entrada < (18 * 60 + 10)) {
+//Si llegó de día
+$valor += 2000;
 } else {
-$max = $minutos_salida;
+//Si llegó de noche
+//$valor += 2500;
 }
-for ($i = $minutos_entrada + 1; $i <= $minutos_salida; $i++) {
-switch ($i) {
-case 7 * 60 + 10:
-$valor += 1400;
-break;
-
-case 18 * 60 + 10:
-$valor += 2500;
-break;
-}
-}*/
-}
-
-/*$datos = $request->all();
-$entrada = Carbon::parse($datos['entrada']);
-$salida = Carbon::parse($datos['salida']);
-$dias_diferencia = $salida->day - $entrada->day;
-$puntos = 24 * 60;
-$minutos_salida = $salida->hour * 60 + $salida->minute;
-$ultimo = $minutos_salida;
-$inicial = 2;
 for ($i = 0; $i <= $dias_diferencia; $i++) {
-for ($j = $inicial; $j < $puntos; $j++) {
-echo $j . ' ';
-if ($j == 2) {
-echo 'dos ';
+for ($j = $inicial + 1; $j < $minutos_dia; $j++) {
+$valor_noche = 0;
+if ((7 * 60 + 10) <= $j && $j < (18 * 60 + 10)) {
+//Si es de día
+if (($j - $minutos_entrada) % 60 == 0 && $valor < 12000) {
+$valor += 2000;
+}
+} else {
+if (($j - $minutos_entrada) % 60 == 0 && $valor_noche < 4000) {
+$valor_noche += 2000;
+}
 }
 if ($i == $dias_diferencia && $j == $ultimo) {
 break;
 }
 }
 $inicial = 0;
-}*/
+$valor += $valor_noche;
+}
+ */
