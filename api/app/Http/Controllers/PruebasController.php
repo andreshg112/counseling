114008cfa8c6eea10847daf881ba97663b5c6ea4
Andrés Herrera García <andreshg112@gmail.com>
2016-08-11 16:23:09 -0500

    Valida correctamente.

--- api/app/Http/Controllers/PruebasController.php
+++ api/app/Http/Controllers/PruebasController.php
@@ -10,14 +10,38 @@ class PruebasController extends Controller
 {
     public function cobrarEnParqueadero(Request $request)
     {
-        $datos = $request->all();
-        $entrada = Carbon::parse($datos['entrada']);
-        $salida = Carbon::parse($datos['salida']);
-        $id_vehiculo = $datos['id_vehiculo'];
-        $valor = 0;
-        $vehiculo = FactoryVehiculo::getVehiculo($id_vehiculo);
-        $valor = $vehiculo->cobrar($entrada, $salida);
-        return $valor;
+        $respuesta = [];
+        if (!is_array($request->all())) {
+            $respuesta['result'] = false;
+            $respuesta['mensaje'] = 'Los datos enviados no tienen el formato correcto.';
+        } else {
+            $rules = [
+                'entrada' => 'required|date',
+                'salida' => 'required|date',
+                'id_vehiculo' => 'required|in:1,2,3,4,5',
+            ];
+            try {
+                $validator = \Validator::make($request->all(), $rules);
+                if ($validator->fails()) {
+                    $respuesta['result'] = false;
+                    $respuesta['validator'] = $validator->errors()->all();
+                    $respuesta['mensaje'] = '¡Error!';
+                } else {
+                    $datos = $request->all();
+                    $entrada = Carbon::parse($datos['entrada']);
+                    $salida = Carbon::parse($datos['salida']);
+                    $id_vehiculo = $datos['id_vehiculo'];
+                    $valor = 0;
+                    $vehiculo = FactoryVehiculo::getVehiculo($id_vehiculo);
+                    $valor = $vehiculo->cobrar($entrada, $salida);
+                    $respuesta['result'] = $valor;
+                }
+            } catch (Exception $e) {
+                $respuesta['result'] = false;
+                $respuesta['mensaje'] = "Error: $e";
+            }
+        }
+        return $respuesta;
     }
 
 }
